005adb330473c45a21f27e2466439cd020e20554
Kunkulis <kunkulis@inbox.lv>
2021-12-04 15:01:14 +0200

    take2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,63 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var draw = File.ReadLines("input.txt").First().Split(",").Select(Int32.Parse).ToList();
+var draw = File.ReadLines("input.txt").First().Split(",").Select(Int32.Parse).ToArray();
 var boardsRaw = File.ReadAllLines("input.txt").Skip(2).ToList();
-var board = new List<int>();
-var boards = new List<List<int>>();
+int[,] board = new int[5, 5];
+var boards = new List<int[,]>();
 
+int row = 0;
 for (int i = 0; i < boardsRaw.Count(); i++)
 {
     var dataString = boardsRaw[i];
     if (dataString == string.Empty)
     {
-        boards.Add(board.ToList());
-        board.Clear();
+        boards.Add(board);
+        board = new int[5, 5];
+        row = 0;
         continue;
     }
     else
     {
-        board.AddRange(dataString.Trim().Split().Select(Int32.Parse).ToList());
+        var intRow = dataString.Trim().Split().Select(n => Convert.ToInt32(n)).ToArray();
+        for (int j = 0; j < intRow.Length; j++)
+        {
+            board[row, j] = intRow[j];
+        }
+        row++;
     }
 }
-
-var bingo = new List<Bingo>();
+boards.Add(board);
 
 foreach (var num in draw)
 {
     for (int i = 0; i < boards.Count; i++)
     {
-        for (int n = 0; n < boards[i].Count; n++)
+        var aBoard = boards[i];
+        for (int j = 0; j < aBoard.Length; j++)
         {
-            if (num == boards[i][n])
-            {                
-                bingo.Insert(i, new BingoItem(n,num)); //cenšos uztaisīt Listu, kur lieku iekšā nosauktos ciparus ar to indexu. Vajadzētu atrast no kuras bingo kartes viņš ir paņemts, tad tajā arī ielikt
+            var luckyNum = aBoard[i,j];
+            if(luckyNum == num)
+            {
+
             }
         }
     }
-    //var items = boards.SelectMany(br=>br).Where(b=>b == num).ToList();
-
-    foreach (var bing in bingo)
-    {
-
-    }
 }
+
+//foreach (var num in draw)
+//{
+//    for (int i = 0; i < boards.Count; i++)
+//    {
+//        for (int n = 0; n < boards[i].Count; n++)
+//        {
+//            //if (num == boards[i][n])
+//            //{                
+//            //    //bingo.Insert(i, new BingoItem(n,num)); //cenšos uztaisīt Listu, kur lieku iekšā nosauktos ciparus ar to indexu. Vajadzētu atrast no kuras bingo kartes viņš ir paņemts, tad tajā arī ielikt
+//            //}
+//        }
+//    }
+//    //var items = boards.SelectMany(br=>br).Where(b=>b == num).ToList();
+
+//    foreach (var bing in bingo)
+//    {
+
+//    }
+//}
 
 Console.ReadKey();
-
-public class BingoItem
-{    
-    public int index;
-    public int number;
-
-    public BingoItem( int n, int num)
-    {
-        index = n;
-        number = num;
-    }
-}
-
-public class Bingo
-{
-    public int board;
-    public BingoItem bingoItem;
-    
-}
