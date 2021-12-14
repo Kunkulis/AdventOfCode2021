b893b1b5ec49e1f94f02808e8d6e8128039c591a
Kunkulis <kunkulis@inbox.lv>
2021-12-14 17:15:21 +0200

    small cleanup

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var inputRaw = File.ReadAllLines("input.txt");
+var inputRaw = File.ReadAllLines("test.txt");
 var polymer = inputRaw[0].Trim();
 
 var links = new Dictionary<string, string>();
@@ -10,15 +10,19 @@ for (int i = 2; i < inputRaw.Length; i++)
     links.Add(splitInput[0].Trim(), splitInput[1].Trim());
 }
 
-var steps = 0;
-
-var pairs = Enumerable.Range(0, polymer.Length - 1).Select((x => polymer[x..(x + 2)]));
-var pairsDic = pairs.GroupBy(x => x).ToDictionary(y => y.Key, y => (long)y.Count());
-
+var pairsDic = Enumerable.Range(0, polymer.Length - 1).Select((x => polymer[x..(x + 2)])).GroupBy(x => x).ToDictionary(y => y.Key, y => (long)y.Count());
 var charCount = polymer.GroupBy(x => x).ToDictionary(y => y.Key, y => (long)y.Count());
 
-while (steps != 40)
-{   
+for (int i = 0; i < 40; i++)
+{
+    pairsDic = Step(pairsDic);       
+}
+
+Console.WriteLine(Result());
+Console.ReadLine();
+
+Dictionary<string, long> Step(Dictionary<string, long> pairsDic)
+{
     var newPairs = new Dictionary<string, long>();
 
     foreach (var item in pairsDic)
@@ -52,12 +56,10 @@ while (steps != 40)
         }
 
     }
-    pairsDic = new Dictionary<string, long>(newPairs);
-    steps++;
+    return newPairs;
 }
 
-var min = charCount.Aggregate((l, r) => l.Value < r.Value ? l : r).Value;
-var max = charCount.Aggregate((l, r) => l.Value > r.Value ? l : r).Value;
-
-Console.WriteLine(max-min);
-Console.ReadLine();
+object Result()
+{
+    return charCount.Aggregate((l, r) => l.Value > r.Value ? l : r).Value - charCount.Aggregate((l, r) => l.Value < r.Value ? l : r).Value;
+}
