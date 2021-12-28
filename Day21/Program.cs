7f2a294174e4488555f6012d49662474351a5152
Kunkulis <kunkulis@inbox.lv>
2021-12-28 08:31:38 +0200

    Update Program.cs

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,5 +1,5 @@
-﻿var player1 = 4;
-var player2 = 8;
+﻿var player1 = 10;
+var player2 = 9;
 
 var dieNum = 1;
 
@@ -8,44 +8,47 @@ var player2Score = 0;
 
 var throws = 0;
 var move = 0;
-var didSomeOneWin = false;
 
-while (!didSomeOneWin)
+while (true)
 {
     (move, dieNum) = ThreeThrows(dieNum);
-    throws += 3;
+    throws+= 3;
 
     var score = int.Parse((player1 + move).ToString().ToCharArray()[^1].ToString());
     player1Score = score == 0 ? player1Score + 10 : player1Score + score;
     player1 = score == 0 ? 10 : score;
-
+    if (player1Score >= 1000)
+    {
+        Console.WriteLine(throws * player2Score);
+        break;
+    }
     (move, dieNum) = ThreeThrows(dieNum);
-    throws += 3;
+    throws+= 3;
 
     score = int.Parse((player2 + move).ToString().ToCharArray()[^1].ToString());
     player2Score = score == 0 ? player2Score + 10 : player2Score + score;
     player2 = score == 0 ? 10 : score;
 
-    if (player1Score>1000||player2Score>1000)
+    if (player2Score >= 1000)
     {
-        didSomeOneWin = true;
+        Console.WriteLine(throws * player1Score);
+        break;
     }
 }
 
-Console.WriteLine(throws * player2Score);
 Console.ReadLine();
 
 (int, int) ThreeThrows(int dieNum)
 {
     var sum = 0;
-    if (dieNum + 3 <= 100)
+    if (dieNum + 2 <= 100)
     {
         return ((dieNum + (dieNum + 1) + (dieNum + 2)), (dieNum + 3));
     }
     else
     {
         var throw1 = dieNum;
-        var throw2 = dieNum++;
+        var throw2 = dieNum + 1;
         var throw3 = dieNum + 2;
 
         if (throw1 <= 100)
@@ -74,9 +77,9 @@ Console.ReadLine();
         else
         {
             dieNum = 1;
-            return ((sum + dieNum), (dieNum++));
+            return ((sum + dieNum), (dieNum+1));
         }
 
-        return (sum, dieNum + 2);
+        return (sum, (dieNum + 2));
     }
 }
